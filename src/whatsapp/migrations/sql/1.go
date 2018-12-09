2647b1bc6982f72b939c35ef98aee4ea3957d68f
azharprabudi <azharprabui@gmail.com>
2018-12-09 10:41:13 +0700

    update sql v1

--- src/whatsapp/migrations/sql/1.go
+++ src/whatsapp/migrations/sql/1.go
@@ -1,11 +1,10 @@
 package wasql
 
 const V1 = `
-CREATE TABLE "user_sessions" (
+CREATE TABLE "sessions" (
 	"id" uuid NOT NULL,
 	"value" text NOT NULL,
 	"created_at" timestampz NOT NULL,
-	"expired_at" timestampz, 
-	CONSTRAINT "user_sessions_pk" PRIMARY KEY ("id"),
+	CONSTRAINT "sessions_pk" PRIMARY KEY ("id"),
 );
 `
